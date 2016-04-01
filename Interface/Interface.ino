6ec5a832f6af3caa67180f25f1fe7790dcba060d
Daniel James <danielcolinjames@gmail.com>
2016-04-01 15:13:49 -0400

    Cleaned up interface Arduino code
    - removed a few lines from testing

--- Interface/Interface.ino
+++ Interface/Interface.ino
@@ -1,36 +1,10 @@
 #include <SoftwareSerial.h>
 
-/*
- * 
- * 79 - Game state:
- *      Number of active suits,
- *      Number of red suits,
- *      Number of blue suits
- *      
- * 78 - Start game:
- *      Number of active suits
- *      
- * 77 - Suit was tagged:
- *      New colour (0 if only 1 person is it)
- *      
- * 76 - Friendly fire:
- *      Suit colour
- *      
- * 75 - Game over:
- *      Winning colour (or 0 if timeout)
- *      
- */
-
-
 SoftwareSerial consoleInterface(3, 2); // rx, tx
 SoftwareSerial debugSerial(8, 7);
 
 int fromConsole;
 uint8_t toConsole;
-
-uint8_t one = 1;
-uint8_t two = 2;
-uint8_t three = 3;
 
 void setup() {
   // Open serial communications and wait for port to open:
@@ -61,10 +35,10 @@ void listenToConsole() {
 
 void listenToMax() {
   if (Serial.available()) {
-    int reading = Serial.read();
+    toConsole = Serial.read();
     debugSerial.print("Message from Max: ");
-    debugSerial.println(reading);
-    consoleInterface.write(reading);
+    debugSerial.println(toConsole);
+    consoleInterface.write(toConsole);
   }
 }
 
